0b4ba721362a6b12465fafcd29ddda7fa92dc53c
Blaise122 <70517861+Blaise122@users.noreply.github.com>
2021-08-05 19:25:42 +1000

    add user to Listing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_05_090334) do
+ActiveRecord::Schema.define(version: 2021_08_05_091828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,9 @@ ActiveRecord::Schema.define(version: 2021_08_05_090334) do
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["category_id"], name: "index_listings_on_category_id"
+    t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_08_05_090334) do
   end
 
   add_foreign_key "listings", "categories"
+  add_foreign_key "listings", "users"
 end
