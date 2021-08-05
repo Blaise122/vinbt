480350e7d15250fcb0d58407526d73213f1b93c4
Blaise122 <70517861+Blaise122@users.noreply.github.com>
2021-08-05 18:25:31 +1000

    created database

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+
+environment.plugins.append('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jQuery',
+        jQuery: 'jQuery',
+        Popper: ['popper.js', 'default']
+    })
+)
+
 module.exports = environment
