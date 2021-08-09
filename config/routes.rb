111d631320890587e1c385a350835076fdbf5699
Blaise122 <70517861+Blaise122@users.noreply.github.com>
2021-08-09 18:22:01 +1000

    generated payment controller and installed webhook

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  get 'payments/webhook'
   devise_for :users
   root to: 'pages#home'
   resources :listings
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  post '/payments/webhook', to: 'payments#webhook'
+  get 'payments/success', to: 'payments#success'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
