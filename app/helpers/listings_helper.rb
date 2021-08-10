c045c2da0400c62df73692e60fc06b4d26be980d
Blaise122 <70517861+Blaise122@users.noreply.github.com>
2021-08-10 15:39:55 +1000

    employed date sanitation, added footer & validation

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -2,7 +2,7 @@ module ListingsHelper
 
     def render_buy_button
         if current_user
-            button = "<button data-stripe=\"payment\" class=\"btn btn-primary p-3\">Buy: $#{@listing.price}</button>"
+            button = "<button data-stripe=\"payment\" class=\"btn btn-success p-3\">Buy: $#{@listing.price}</button>"
         else
             button = "<button class=\"btn btn-secondary\" disabled>Log In to Buy</button>"
         end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,4 +2,22 @@ class Listing < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many_attached :picture
+  before_save :remove_whitespace
+end
+
+validates :name, :price,:description, :size, :picture, presence: true
+  #this method to call the first image in the image array to show since  a listing can have many pictures.
+
+def listing_attachment 
+  picture[0]
+end
+
+
+private
+
+  def remove_whitespace
+    self.name = self.name.strip
+    self.description = self.description.strip
+  end
+
 end
